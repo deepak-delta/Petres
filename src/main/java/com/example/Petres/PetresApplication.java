56fd453c9342f746dbb55bcad3b423d5ddff06f5
deepak-delta <ddk3857@gmail.com>
2023-12-09 15:57:19 +0530

    Initial Commit

---
+++ src/main/java/com/example/Petres/PetresApplication.java
@@ -0,0 +1,13 @@
+package com.example.Petres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetresApplication.class, args);
+	}
+
+}
